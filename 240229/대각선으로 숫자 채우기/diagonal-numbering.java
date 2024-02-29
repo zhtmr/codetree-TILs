a085c0ecd49b9e46cbccf859bda1db775e690c09
csw <48509269+zhtmr@users.noreply.github.com>
2024-02-29 21:33:27 +0900

    Title: 대각선으로 숫자 채우기, Time: 209ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240229/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+
+    int n = scan.nextInt();
+    int m = scan.nextInt();
+    int[][] arr = new int[n][m];
+    int count = 0;
+    int nth = 0;
+    int add = 0;
+
+    while (count != n * m) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          if (i + j == nth) {
+            arr[i][j] = ++add;
+            count++;
+          }
+        }
+      }
+      nth++;
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.print(arr[i][j] +" ");
+      }
+      System.out.println();
+    }
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
